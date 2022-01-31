8257ee5e1b05578c98747b05baa98fb81260a2af
Marcel Tiator <tiator.ma@gmail.com>
2022-01-31 23:06:40 +0100

    added test for nn only

--- compare.py
+++ compare.py
@@ -40,35 +40,46 @@ def save_csv(res, csv_dir, csv_name, data_header):
     write_csv(filedir=csv_dir, filename=csv_name, csv=csv)
 
 
-def ooa(par_v_gt, par_v_M, par_v_M_k, par_v_M_ks, par_v_P):
+def ooa(par_v_gt, par_v_M, par_v_M_k, par_v_M_ks, par_v_P, nn_only):
     sortation = np.argsort(par_v_gt)
     par_v_gt = par_v_gt[sortation]
     par_v_M = par_v_M[sortation]
-    par_v_M_k = par_v_M_k[sortation]
-    par_v_M_ks = par_v_M_ks[sortation]
+    if not nn_only:
+        par_v_M_k = par_v_M_k[sortation]
+        par_v_M_ks = par_v_M_ks[sortation]
+    
     par_v_P = par_v_P[sortation]
 
     ugt, ugt_idxs, ugt_counts = np.unique(par_v_gt, return_index=True, return_counts=True)
 
     partition_gt = Partition(partition=par_v_gt, uni=ugt, idxs=ugt_idxs, counts=ugt_counts)
     partition_M = Partition(partition=par_v_M)
-    partition_M_k = Partition(partition=par_v_M_k)
-    partition_M_ks = Partition(partition=par_v_M_ks)
+    
+    if not nn_only:
+        partition_M_k = Partition(partition=par_v_M_k)
+        partition_M_ks = Partition(partition=par_v_M_ks)
+    
     partition_P = Partition(partition=par_v_P)
 
     # nr of vertices/points
     max_density = partition_gt.partition.shape[0]
 
     ooa_M, _ = partition_gt.overall_obj_acc(max_density=max_density, partition_B=partition_M, density_function=densities_np)
-    ooa_M_k, _ = partition_gt.overall_obj_acc(max_density=max_density, partition_B=partition_M_k, density_function=densities_np)
-    ooa_M_ks, _ = partition_gt.overall_obj_acc(max_density=max_density, partition_B=partition_M_ks, density_function=densities_np)
+    
+    if not nn_only:
+        ooa_M_k, _ = partition_gt.overall_obj_acc(max_density=max_density, partition_B=partition_M_k, density_function=densities_np)
+        ooa_M_ks, _ = partition_gt.overall_obj_acc(max_density=max_density, partition_B=partition_M_ks, density_function=densities_np)
+    
     ooa_P, _ = partition_gt.overall_obj_acc(max_density=max_density, partition_B=partition_P, density_function=densities_np)
 
-    return ooa_M, ooa_M_k, ooa_M_ks, ooa_P
+    if nn_only:
+        return ooa_M, None, None, ooa_P
+    else:
+        return ooa_M, ooa_M_k, ooa_M_ks, ooa_P
 
 
 def compare(comp_args):
-    scene_id, scene_name, scannet_dir, reg_strength, k_nn_adj, partition_dir = comp_args
+    scene_id, scene_name, scannet_dir, reg_strength, k_nn_adj, partition_dir, nn_only = comp_args
     #scene_name = "scene0085_01"
     lambda_edge_weight = 1.
     d_se_max = 0
@@ -101,45 +112,46 @@ def compare(comp_args):
         #return None
         par_v_M = initial_partition(P=mesh, sp_idxs=sp_idxs_M, verbose=False)
 
-        #print("2")
-        n_sps_M_k, n_sedg_M_k, sp_idxs_M_k, senders_M_k, receivers_M_k, stris_M_k, stats_M_k = superpoint_graph_mesh(
-            mesh_vertices_xyz=mesh.vertices,
-            mesh_vertices_rgb=mesh.vertex_colors,
-            mesh_tris=mesh.triangles,
-            adj_list=mesh.adjacency_list,
-            lambda_edge_weight=lambda_edge_weight,
-            reg_strength=reg_strength,
-            d_se_max=d_se_max,
-            k_nn_adj=k_nn_adj,
-            use_cartesian=False,
-            n_proc=1,
-            ignore_knn=True,
-            smooth=False,
-            verbose=False,
-            g_dir="./tmp",
-            g_filename=scene_name
-            )
-        par_v_M_k = initial_partition(P=mesh, sp_idxs=sp_idxs_M_k, verbose=False)
-        
-        #print("3")
-        n_sps_M_ks, n_sedg_M_ks, sp_idxs_M_ks, senders_M_ks, receivers_M_ks, stris_M_ks, stats_M_ks = superpoint_graph_mesh(
-            mesh_vertices_xyz=mesh.vertices,
-            mesh_vertices_rgb=mesh.vertex_colors,
-            mesh_tris=mesh.triangles,
-            adj_list=mesh.adjacency_list,
-            lambda_edge_weight=lambda_edge_weight,
-            reg_strength=reg_strength,
-            d_se_max=d_se_max,
-            k_nn_adj=k_nn_adj,
-            use_cartesian=False,
-            n_proc=1,
-            ignore_knn=True,
-            smooth=True,
-            verbose=False,
-            g_dir="./tmp",
-            g_filename=scene_name
-            )
-        par_v_M_ks = initial_partition(P=mesh, sp_idxs=sp_idxs_M_ks, verbose=False)
+        if not nn_only:
+            #print("2")
+            n_sps_M_k, n_sedg_M_k, sp_idxs_M_k, senders_M_k, receivers_M_k, stris_M_k, stats_M_k = superpoint_graph_mesh(
+                mesh_vertices_xyz=mesh.vertices,
+                mesh_vertices_rgb=mesh.vertex_colors,
+                mesh_tris=mesh.triangles,
+                adj_list=mesh.adjacency_list,
+                lambda_edge_weight=lambda_edge_weight,
+                reg_strength=reg_strength,
+                d_se_max=d_se_max,
+                k_nn_adj=k_nn_adj,
+                use_cartesian=False,
+                n_proc=1,
+                ignore_knn=True,
+                smooth=False,
+                verbose=False,
+                g_dir="./tmp",
+                g_filename=scene_name
+                )
+            par_v_M_k = initial_partition(P=mesh, sp_idxs=sp_idxs_M_k, verbose=False)
+            
+            #print("3")
+            n_sps_M_ks, n_sedg_M_ks, sp_idxs_M_ks, senders_M_ks, receivers_M_ks, stris_M_ks, stats_M_ks = superpoint_graph_mesh(
+                mesh_vertices_xyz=mesh.vertices,
+                mesh_vertices_rgb=mesh.vertex_colors,
+                mesh_tris=mesh.triangles,
+                adj_list=mesh.adjacency_list,
+                lambda_edge_weight=lambda_edge_weight,
+                reg_strength=reg_strength,
+                d_se_max=d_se_max,
+                k_nn_adj=k_nn_adj,
+                use_cartesian=False,
+                n_proc=1,
+                ignore_knn=True,
+                smooth=True,
+                verbose=False,
+                g_dir="./tmp",
+                g_filename=scene_name
+                )
+            par_v_M_ks = initial_partition(P=mesh, sp_idxs=sp_idxs_M_ks, verbose=False)
 
         #print("4")
         P, xyz, rgb = get_P(mesh=mesh)
@@ -159,7 +171,8 @@ def compare(comp_args):
             par_v_M=np.array(par_v_M, copy=True),
             par_v_M_k=np.array(par_v_M_k, copy=True),
             par_v_M_ks=np.array(par_v_M_ks, copy=True),
-            par_v_P=np.array(par_v_P, copy=True))
+            par_v_P=np.array(par_v_P, copy=True),
+            nn_only=nn_only)
 
         adj_list_ = mesh.adjacency_list.copy()
         n_per_v = np.zeros((len(adj_list_), ), np.uint32)
@@ -168,16 +181,25 @@ def compare(comp_args):
             n_per_v[i] = len(al)
 
         partition_file = str(scene_id) + "_" + str(reg_strength) + "_" + str(k_nn_adj)
-        save_partitions(partitions=[("gt", par_v_gt), ("M", par_v_M), ("P", par_v_P), ("M_k", par_v_M_k)],
-            fdir=partition_dir, fname=partition_file, verbose=False)
+        if nn_only:
+            partitions = [("gt", par_v_gt), ("M", par_v_M), ("P", par_v_P)]
+        else:
+            partitions = [("gt", par_v_gt), ("M", par_v_M), ("P", par_v_P), ("M_k", par_v_M_k), ("M_ks", par_v_M_ks)]
+        save_partitions(partitions=partitions, fdir=partition_dir, fname=partition_file, verbose=False)
     except Exception as e:
         print(e)
         traceback.print_exc()
         return None
 
-    return scene_id, scene_name, P.shape[0], np.asarray(mesh.triangles).shape[0], reg_strength, k_nn_adj,\
-        n_sps_M, n_sedg_M, ooa_M, n_sps_M_k, n_sedg_M_k, ooa_M_k, n_sps_M_ks, n_sedg_M_ks, ooa_M_ks, n_sps_P, n_sedg_P, ooa_P,\
-        np.mean(n_per_v), np.std(n_per_v), np.median(n_per_v), file_gt, partition_file, stats_M, stats_M_k, stats_M_ks, stats_P
+    if nn_only:
+        return scene_id, scene_name, P.shape[0], np.asarray(mesh.triangles).shape[0], reg_strength, k_nn_adj,\
+            n_sps_M, n_sedg_M, ooa_M, n_sps_P, n_sedg_P, ooa_P,\
+            np.mean(n_per_v), np.std(n_per_v), np.median(n_per_v), file_gt, partition_file, stats_M, stats_P
+    else:
+        return scene_id, scene_name, P.shape[0], np.asarray(mesh.triangles).shape[0], reg_strength, k_nn_adj,\
+            n_sps_M, n_sedg_M, ooa_M, n_sps_M_k, n_sedg_M_k, ooa_M_k, n_sps_M_ks, n_sedg_M_ks, ooa_M_ks, n_sps_P, n_sedg_P, ooa_P,\
+            np.mean(n_per_v), np.std(n_per_v), np.median(n_per_v), file_gt, partition_file, stats_M, stats_M_k, stats_M_ks, stats_P
+    
 
 
 def main():
@@ -188,6 +210,7 @@ def main():
     parser.add_argument("--n_proc", default=1, type=int, help="Number of processes that will be used.")
     parser.add_argument("--pkg_size", default=100, type=int, help="Number of packages to save a csv")
     parser.add_argument("--offset", default=0, type=int, help="Offset for the name of the csv file.")
+    parser.add_argument("--nn_only", default=False, type=bool, help="If True, only geodesic and euclidean nearest neighbours will be calculated.")
     args = parser.parse_args()
     mkdir(args.partition_dir)
     mkdir(args.csv_dir)
@@ -202,13 +225,18 @@ def main():
         "knn", # neighbourhood size which is considered in the CP
         "|S_M|", # nr of superpoints in the mesh partition
         "|E_M|", # nr of superedges in the mesh superpoint graph
-        "OOA_M", # overall object accuracy of the mesh
-        "|S_M_k|", # nr of superpoints in the mesh partition
-        "|E_M_k|", # nr of superedges in the mesh superpoint graph
-        "OOA_M_k", # overall object accuracy of the mesh
-        "|S_M_ks|", # nr of superpoints in the mesh partition
-        "|E_M_ks|", # nr of superedges in the mesh superpoint graph
-        "OOA_M_ks", # overall object accuracy of the mesh
+        "OOA_M" # overall object accuracy of the mesh
+    ]
+    if not args.nn_only:
+        data_header.extend([
+            "|S_M_k|", # nr of superpoints in the mesh partition
+            "|E_M_k|", # nr of superedges in the mesh superpoint graph
+            "OOA_M_k", # overall object accuracy of the mesh
+            "|S_M_ks|", # nr of superpoints in the mesh partition
+            "|E_M_ks|", # nr of superedges in the mesh superpoint graph
+            "OOA_M_ks", # overall object accuracy of the mesh
+        ])
+    data_header.extend([
         "|S_P|", # nr of superpoints in the point cloud partition
         "|E_P|", # nr of superedges in the point cloud superpoint graph
         "OOA_P", # overall object accuracy of the point cloud
@@ -217,9 +245,14 @@ def main():
         "median(n)", # median of neighbours per vertex
         "file_gt", # path to the ground truth mesh
         "partitions_file" # path to a file where the partitions are stored
-        ]
+    ])
 
-    for post in ["_M", "_M_k", "_M_ks", "_P"]:
+    if args.nn_only:
+        posts = ["_M", "_P"]
+    else:
+        posts = ["_M", "_M_k", "_M_ks", "_P"]
+
+    for post in posts:
         data_header.append("max_ite" + post)
         data_header.append("n_ite" + post)
         data_header.append("exit_code" + post)
@@ -241,6 +274,18 @@ def main():
         data_header.append("e2_3" + post)
         data_header.append("e2_4" + post)
         data_header.append("e2_5" + post)
+
+        data_header.append("|SSp1|" + post)
+        data_header.append("|SSp2|" + post)
+        data_header.append("|SSp3|" + post)
+        data_header.append("|SSp4|" + post)
+        data_header.append("|SSp5|" + post)
+
+        data_header.append("|SR1|" + post)
+        data_header.append("|SR2|" + post)
+        data_header.append("|SR3|" + post)
+        data_header.append("|SR4|" + post)
+        data_header.append("|SR5|" + post)
     knns = [30, 60]
     reg_strengths = [1]
 
@@ -266,13 +311,13 @@ def main():
         for cp_id in range(n_cp_args):
             #idx = n_cp_args * scene_id + cp_id
             reg_strength, k_nn_adj = cp_args[cp_id]
-            comp_args[idx] = (scene_id, scene_name, scannet_dir, reg_strength, k_nn_adj, args.partition_dir)
+            comp_args[idx] = (scene_id, scene_name, scannet_dir, reg_strength, k_nn_adj, args.partition_dir, args.nn_only)
             idx += 1
     if args.n_proc == 1:
         res = []
         for i in tqdm(range(len(comp_args)), desc="Compare"):
             comp_arg = comp_args[i] 
-            scene_id, scene_name, scannet_dir, reg_strength, k_nn_adj, p_dir = comp_arg
+            scene_id, scene_name, scannet_dir, reg_strength, k_nn_adj, p_dir, nn_only = comp_arg
             r = compare(comp_args=comp_arg)
             #print("result:", r)
             if r is None:
