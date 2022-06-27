59ebd6d8ec34dfb46f3a4ca3a8ce8b4e497c780a
Marcel Tiator <marcel.tiator@hs-duesseldorf.de>
2022-06-27 16:34:50 +0200

    TODO add method to analyze all scenes

--- fh04_vary_k.py
+++ fh04_vary_k.py
@@ -9,7 +9,6 @@ from ai_utils import graph, predict
 from partition.felzenszwalb import partition_from_probs
 from partition.partition import Partition
 from partition.density_utils import densities_np
-# try to iterate over all scenes with $K \in \{0.001, 0.1, 1, 1.5, 10\}$
 
 
 def mkdir(directory):
@@ -186,6 +185,74 @@ def main():
     #plt.xticks(k_params)
     plt.savefig(fname="fh04_k_sizes_id{0}.jpg".format(args.sn_id))
     plt.show()
+
+
+def all_scenes():
+    parser = argparse.ArgumentParser()
+    # cp params
+    parser.add_argument("--reg_strength", default=0.1, type=float, help="Regularization strength for the minimal partition. Increase lambda for a coarser partition. ")
+    parser.add_argument("--k_nn_geof", default=45, type=int, help="Number of neighbors for the geometric features.")
+    parser.add_argument("--k_nn_adj", default=10, type=int, help="Adjacency structure for the minimal partition.")
+    parser.add_argument("--lambda_edge_weight", default=1., type=float, help="Parameter determine the edge weight for minimal part.")
+    parser.add_argument("--d_se_max", default=0, type=float, help="Max length of super edges.")
+    parser.add_argument("--max_sp_size", default=-1, type=int, help="Maximum size of a superpoint.")
+    #
+    parser.add_argument("--pkg_size", default=5, type=int, help="Number of packages to save a csv")
+    parser.add_argument("--csv_dir", default="./", type=str, help="Directory where we save the csv.")
+    args = parser.parse_args()
+
+    k_params = [0.001, 0.1, 1, 2, 3, 4, 10]
+
+    max_sn_id = 707
+    min_sn_id = 0
+    if args.sn_id > max_sn_id or args.sn_id < min_sn_id:
+        raise Exception("Scannet ID {0} should be between {1} and {2}.".format(args.sn_id, min_sn_id, max_sn_id))
+    scenes, _, scannet_dir = get_scenes(blacklist=[])
+    scene_name = scenes[args.sn_id]
+
+    csv_header = get_csv_header()
+    csv_name = "fh04_k_id{0}".format(args.sn_id)
+    
+    verbose = False
+    model = None
+
+    mesh, p_vec_gt, file_gt = get_ground_truth(scannet_dir=scannet_dir, scene=scene_name)
+    xyz = np.asarray(mesh.vertices)
+    rgb = np.asarray(mesh.vertex_colors)
+    P = np.hstack((xyz, rgb))
+
+    graph_dict, sp_idxs, part_cp = graph(
+        cloud=P,
+        k_nn_adj=args.k_nn_adj,
+        k_nn_geof=args.k_nn_geof,
+        lambda_edge_weight=args.lambda_edge_weight,
+        reg_strength=args.reg_strength,
+        d_se_max=args.d_se_max,
+        max_sp_size=args.max_sp_size,
+        verbose=verbose,
+        return_p_vec=True)
+
+    unions, probs, model = predict(graph_dict=graph_dict, dec_b=0.8, return_model=True, verbose=False)
+
+    ooas = []
+    sizes = []
+    all_data = []
+    partition_gt = None
+    sortation = None
+
+    ooa_cp, partition_gt, sortation = ooa(par_v_gt=p_vec_gt, par_v=part_cp, partition_gt=partition_gt, sortation=sortation)
+    size_cp = len(sp_idxs)
+
+    for i in tqdm(range(k_params.shape[0]), desc="Vary K of FH04", disable=verbose):
+        k = k_params[i]
+        part_fh04 = partition_from_probs(graph_dict=graph_dict, sim_probs=probs, k=k, P=P, sp_idxs=sp_idxs)
+        ooa_fh04, partition_gt, sortation = ooa(par_v_gt=p_vec_gt, par_v=part_fh04, partition_gt=partition_gt, sortation=sortation)
+        size_fh04 = np.unique(part_fh04).shape[0]
+        ooas.append(ooa_fh04)
+        sizes.append(size_fh04)
+        all_data.append([scene_name, k, ooa_fh04, size_fh04, ooa_cp, size_cp])
+    
+    save_csv(res=all_data, csv_dir=args.csv_dir, csv_name=csv_name, data_header=csv_header)
 
 
 if __name__ == "__main__":
